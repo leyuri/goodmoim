d240d8f08012f69babcea5978e9ee8749ae037b5
leyuri <leyuri97@gmail.com>
2020-12-18 10:05:25 +0900

    Create project

---
+++ src/test/java/com/goodmoim/GoodmoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodmoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodmoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
