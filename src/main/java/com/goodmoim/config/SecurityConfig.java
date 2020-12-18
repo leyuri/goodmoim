61913d3c6f4e68e130503ceb15f0d93f02bdb769
leyuri <leyuri97@gmail.com>
2020-12-18 12:35:54 +0900

    Add SignUp view

--- src/main/java/com/goodmoim/config/SecurityConfig.java
+++ src/main/java/com/goodmoim/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package com.goodmoim.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // 일단 메모리에 떠야 하니까 어노테이션 추가해줌
+@EnableWebSecurity // 직접 시큐리티 설정을 하겠다는 의미
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -17,5 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring() //static resources은 스프링 시큐리티 필터를 적용X
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
