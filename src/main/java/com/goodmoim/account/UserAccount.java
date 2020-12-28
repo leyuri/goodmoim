52df641acce6386013560836fabec54d51455ee7
leyuri <leyuri97@gmail.com>
2020-12-28 12:20:29 +0900

    Add CurrentUser annotation

---
+++ src/main/java/com/goodmoim/account/UserAccount.java
@@ -0,0 +1,19 @@
+package com.goodmoim.account;
+
+import com.goodmoim.domain.Account;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class UserAccount extends User {
+
+    private Account account;
+
+    public UserAccount(Account account) {
+        super(account.getNickname(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        this.account = account; // 설정을 꼭 해줘야 함..
+    }
+}
