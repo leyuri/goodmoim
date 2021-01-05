22e636a248e80bd1a12a8e749ddb76ca6172f141
leyuri <leyuri97@gmail.com>
2021-01-05 12:17:52 +0900

    Add UpdateProfile

--- src/main/java/com/goodmoim/settings/Profile.java
+++ src/main/java/com/goodmoim/settings/Profile.java
@@ -2,8 +2,10 @@ package com.goodmoim.settings;
 
 import com.goodmoim.domain.Account;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Profile {
 
     private String bio;
